6a2e02a305fdfc3b33693ab5a366f48031c29688
Ivana <ivana.gyro@gmail.com>
2022-03-04 00:06:43 +0800

    Download accident data of TYC
    Add cheerio for parsing HTML.

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
-import got from 'got';
-const { data } = await got.post('https://httpbin.org/anything', {
-    json: {
-        hello: 'world'
-    }
-}).json();
-console.log(data);
+import got from "got";
+// ES6 or TypeScript:
+import cheerio from "cheerio";
+import stream from "node:stream";
+import fs from 'fs';
+import { promisify } from "node:util";
+const baseURI = "https://data.tycg.gov.tw";
+// TODO: recursively create folder
+const baseDir = "data/tyc/";
+fs.mkdirSync(baseDir, { recursive: true });
+const data = await got(`${baseURI}/opendata/datalist/search?page=0&organize=380130000C&allText=桃園市交通事故資料表`, {
+    http2: true,
+}).text();
+const $ = cheerio.load(data);
+const dataPageLinks = $("li.list-group-item").map((_, el) => {
+    return $("a", el).first().attr("href");
+});
+async function downloadCSV(dataPageLink) {
+    const page = await got(`${baseURI}${dataPageLink}`, {
+        http2: true,
+    }).text();
+    const $page = cheerio.load(page);
+    const downloadLinkElement = $page('a[title$=".csv"][href*="/download?"]');
+    const downloadLink = downloadLinkElement.attr('href');
+    const filename = downloadLinkElement.attr('title');
+    const pipeline = promisify(stream.pipeline);
+    console.log(`downloading ${filename}`);
+    await pipeline(got.stream(`${baseURI}${downloadLink}`), fs.createWriteStream(`${baseDir}${filename}`));
+}
+await Promise.all(dataPageLinks.map((_, link) => downloadCSV(link)));
+// const downloadStream = got.stream();
--- index.ts
+++ index.ts
@@ -1,9 +1,46 @@
-import got from 'got';
+import got from "got";
+import cheerio from "cheerio";
+import stream from "node:stream";
+import fs from 'fs';
+import { promisify } from "node:util";
 
-const {data} = await got.post('https://httpbin.org/anything', {
-	json: {
-		hello: 'world'
-	}
-}).json();
+/**
+ * This script is for downloading accident data of TYC.
+ */
 
-console.log(data);
+const baseURI = "https://data.tycg.gov.tw";
+const baseDir = "data/tyc/";
+fs.mkdirSync(baseDir, { recursive: true });
+
+const data = await got(
+  `${baseURI}/opendata/datalist/search?page=0&organize=380130000C&allText=桃園市交通事故資料表`,
+  {
+    http2: true,
+  }
+).text();
+
+const $ = cheerio.load(data);
+const dataPageLinks = $("li.list-group-item").map((_, el) => {
+  return $("a", el).first().attr("href");
+});
+
+async function downloadCSV(dataPageLink: string) {
+  const page = await got(`${baseURI}${dataPageLink}`, {
+    http2: true,
+  }).text();
+
+  const $page = cheerio.load(page);
+
+  const downloadLinkElement = $page('a[title$=".csv"][href*="/download?"]');
+  const downloadLink = downloadLinkElement.attr('href');
+  const filename = downloadLinkElement.attr('title');
+
+  const pipeline = promisify(stream.pipeline);
+  console.log(`downloading ${filename}`);
+  await pipeline(
+    got.stream(`${baseURI}${downloadLink}`),
+    fs.createWriteStream(`${baseDir}${filename}`)
+  );
+}
+
+await Promise.all(dataPageLinks.map((_, link) => downloadCSV(link)));
