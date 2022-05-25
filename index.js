e8e14ae8ae853972fe8b2e3d27e1a02ce647c78e
Ivana <ivana.gyro@gmail.com>
2022-05-25 21:17:47 +0800

    Clean up `index.ts`
    Put the example code into functions so we don't need to comment out the
    code when it is not needed in the working commit.
    
    Issue: #9

--- index.js
+++ index.js
@@ -1,50 +1,60 @@
+/* eslint-disable no-console */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import util from 'util';
+import TYCCrawler from './src/crawlers/TYCCrawler';
 import TYCParser from './src/parsers/TYCParser';
+import { generateShapeOfFields } from './src/tools/shapeOfFields';
 import CSVExporter from './src/exporters/CSVExporter';
 const dataDir = 'data';
 const outputsDir = 'outputs';
-// TYCCrawler.downloadAll(dataDir);
-const allCases = await Promise.all([
-    TYCParser.parseCSV(`${dataDir}/tyc/101.csv`),
-    TYCParser.parseCSV(`${dataDir}/tyc/102.csv`),
-    TYCParser.parseCSV(`${dataDir}/tyc/traffic_accident_103.csv`),
-    TYCParser.parseCSV(`${dataDir}/tyc/traffic_accident_104.csv`),
-    TYCParser.parseCSV(`${dataDir}/tyc/traffic_accident_105.csv`),
-    // TYCParser.parseCSV(`${dataDir}/tyc/traffic106_10809_fix.csv`),
-    // TYCParser.parseCSV(`${dataDir}/tyc/traffic109_fix.csv`),
-    // TYCParser.parseCSV(`${dataDir}/tyc/traffic11011_fix.csv`),
-]);
-CSVExporter.export(`${outputsDir}/tyc/tyc-2012.csv`, allCases[0], 2);
-CSVExporter.export(`${outputsDir}/tyc/tyc-2013.csv`, allCases[1], 2);
-CSVExporter.export(`${outputsDir}/tyc/tyc-2014.csv`, allCases[2], 2);
-CSVExporter.export(`${outputsDir}/tyc/tyc-2015.csv`, allCases[3], 2);
-CSVExporter.export(`${outputsDir}/tyc/tyc-2016.csv`, allCases[4], 2);
-const excludedFields = new Set([
-    '村里',
-    '鄰',
-    '街道',
-    '段',
-    '巷',
-    '弄',
-    '號',
-    '公尺處',
-    '街道1',
-    '段1',
-    '側',
-    '附近',
-    '道路',
-    '公里',
-    '公尺處1',
-    '向',
-    '車道',
-    '平交道',
-    '公里1',
-    '公尺處2',
-    '附近1',
-]);
-const shapeOfFields = {};
-// await generateShapeOfFields(`${outDir}101.csv`, excludedFields, shapeOfFields);
-// await generateShapeOfFields(`${outDir}102.csv`, excludedFields, shapeOfFields);
-// await generateShapeOfFields(`${outDir}traffic_accident_103.csv`, excludedFields, shapeOfFields);
-// await generateShapeOfFields(`${outDir}traffic_accident_104.csv`, excludedFields, shapeOfFields);
-// await generateShapeOfFields(`${outDir}traffic_accident_105.csv`, excludedFields, shapeOfFields);
-// console.log(util.inspect(shapeOfFields, { showHidden: false, depth: null, colors: true }));
+async function downloadTYCData() {
+    return TYCCrawler.downloadAll(dataDir);
+}
+async function parseAndExportTYCDate() {
+    const allCases = await Promise.all([
+        TYCParser.parseCSV(`${dataDir}/tyc/101.csv`),
+        TYCParser.parseCSV(`${dataDir}/tyc/102.csv`),
+        TYCParser.parseCSV(`${dataDir}/tyc/traffic_accident_103.csv`),
+        TYCParser.parseCSV(`${dataDir}/tyc/traffic_accident_104.csv`),
+        TYCParser.parseCSV(`${dataDir}/tyc/traffic_accident_105.csv`),
+        // not support yet
+        // TYCParser.parseCSV(`${dataDir}/tyc/traffic106_10809_fix.csv`),
+        // TYCParser.parseCSV(`${dataDir}/tyc/traffic109_fix.csv`),
+        // TYCParser.parseCSV(`${dataDir}/tyc/traffic11011_fix.csv`),
+    ]);
+    CSVExporter.export(`${outputsDir}/tyc/tyc-2012_2016.csv`, [].concat(...allCases), 
+    /* partyNumber= */ 2);
+}
+async function generateTYCShapeOfFields() {
+    const outDir = `${outputsDir}/tyc/`;
+    const excludedFields = new Set([
+        '村里',
+        '鄰',
+        '街道',
+        '段',
+        '巷',
+        '弄',
+        '號',
+        '公尺處',
+        '街道1',
+        '段1',
+        '側',
+        '附近',
+        '道路',
+        '公里',
+        '公尺處1',
+        '向',
+        '車道',
+        '平交道',
+        '公里1',
+        '公尺處2',
+        '附近1',
+    ]);
+    const shapeOfFields = {};
+    await generateShapeOfFields(`${outDir}101.csv`, excludedFields, shapeOfFields);
+    await generateShapeOfFields(`${outDir}102.csv`, excludedFields, shapeOfFields);
+    await generateShapeOfFields(`${outDir}traffic_accident_103.csv`, excludedFields, shapeOfFields);
+    await generateShapeOfFields(`${outDir}traffic_accident_104.csv`, excludedFields, shapeOfFields);
+    await generateShapeOfFields(`${outDir}traffic_accident_105.csv`, excludedFields, shapeOfFields);
+    console.log(util.inspect(shapeOfFields, { showHidden: false, depth: null, colors: true }));
+}
--- index.ts
+++ index.ts
@@ -1,3 +1,5 @@
+/* eslint-disable no-console */
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import util from 'util';
 
 import TYCCrawler from './src/crawlers/TYCCrawler';
@@ -8,53 +10,62 @@ import CSVExporter from './src/exporters/CSVExporter';
 const dataDir = 'data';
 const outputsDir = 'outputs';
 
-// TYCCrawler.downloadAll(dataDir);
-const allCases = await Promise.all([
-  TYCParser.parseCSV(`${dataDir}/tyc/101.csv`),
-  TYCParser.parseCSV(`${dataDir}/tyc/102.csv`),
-  TYCParser.parseCSV(`${dataDir}/tyc/traffic_accident_103.csv`),
-  TYCParser.parseCSV(`${dataDir}/tyc/traffic_accident_104.csv`),
-  TYCParser.parseCSV(`${dataDir}/tyc/traffic_accident_105.csv`),
-  // TYCParser.parseCSV(`${dataDir}/tyc/traffic106_10809_fix.csv`),
-  // TYCParser.parseCSV(`${dataDir}/tyc/traffic109_fix.csv`),
-  // TYCParser.parseCSV(`${dataDir}/tyc/traffic11011_fix.csv`),
-]);
+async function downloadTYCData() {
+  return TYCCrawler.downloadAll(dataDir);
+}
 
-CSVExporter.export(`${outputsDir}/tyc/tyc-2012.csv`, allCases[0], 2);
-CSVExporter.export(`${outputsDir}/tyc/tyc-2013.csv`, allCases[1], 2);
-CSVExporter.export(`${outputsDir}/tyc/tyc-2014.csv`, allCases[2], 2);
-CSVExporter.export(`${outputsDir}/tyc/tyc-2015.csv`, allCases[3], 2);
-CSVExporter.export(`${outputsDir}/tyc/tyc-2016.csv`, allCases[4], 2);
+async function parseAndExportTYCDate() {
+  const allCases = await Promise.all([
+    TYCParser.parseCSV(`${dataDir}/tyc/101.csv`),
+    TYCParser.parseCSV(`${dataDir}/tyc/102.csv`),
+    TYCParser.parseCSV(`${dataDir}/tyc/traffic_accident_103.csv`),
+    TYCParser.parseCSV(`${dataDir}/tyc/traffic_accident_104.csv`),
+    TYCParser.parseCSV(`${dataDir}/tyc/traffic_accident_105.csv`),
+    // not support yet
+    // TYCParser.parseCSV(`${dataDir}/tyc/traffic106_10809_fix.csv`),
+    // TYCParser.parseCSV(`${dataDir}/tyc/traffic109_fix.csv`),
+    // TYCParser.parseCSV(`${dataDir}/tyc/traffic11011_fix.csv`),
+  ]);
 
-const excludedFields = new Set([
-  '村里',
-  '鄰',
-  '街道',
-  '段',
-  '巷',
-  '弄',
-  '號',
-  '公尺處',
-  '街道1',
-  '段1',
-  '側',
-  '附近',
-  '道路',
-  '公里',
-  '公尺處1',
-  '向',
-  '車道',
-  '平交道',
-  '公里1',
-  '公尺處2',
-  '附近1',
-]);
+  CSVExporter.export(
+    `${outputsDir}/tyc/tyc-2012_2016.csv`,
+    ([] as typeof allCases[0]).concat(...allCases),
+    /* partyNumber= */ 2,
+  );
+}
 
-const shapeOfFields: ShapeOfFields = {};
+async function generateTYCShapeOfFields() {
+  const outDir = `${outputsDir}/tyc/`;
+  const excludedFields = new Set([
+    '村里',
+    '鄰',
+    '街道',
+    '段',
+    '巷',
+    '弄',
+    '號',
+    '公尺處',
+    '街道1',
+    '段1',
+    '側',
+    '附近',
+    '道路',
+    '公里',
+    '公尺處1',
+    '向',
+    '車道',
+    '平交道',
+    '公里1',
+    '公尺處2',
+    '附近1',
+  ]);
 
-// await generateShapeOfFields(`${outDir}101.csv`, excludedFields, shapeOfFields);
-// await generateShapeOfFields(`${outDir}102.csv`, excludedFields, shapeOfFields);
-// await generateShapeOfFields(`${outDir}traffic_accident_103.csv`, excludedFields, shapeOfFields);
-// await generateShapeOfFields(`${outDir}traffic_accident_104.csv`, excludedFields, shapeOfFields);
-// await generateShapeOfFields(`${outDir}traffic_accident_105.csv`, excludedFields, shapeOfFields);
-// console.log(util.inspect(shapeOfFields, { showHidden: false, depth: null, colors: true }));
+  const shapeOfFields: ShapeOfFields = {};
+
+  await generateShapeOfFields(`${outDir}101.csv`, excludedFields, shapeOfFields);
+  await generateShapeOfFields(`${outDir}102.csv`, excludedFields, shapeOfFields);
+  await generateShapeOfFields(`${outDir}traffic_accident_103.csv`, excludedFields, shapeOfFields);
+  await generateShapeOfFields(`${outDir}traffic_accident_104.csv`, excludedFields, shapeOfFields);
+  await generateShapeOfFields(`${outDir}traffic_accident_105.csv`, excludedFields, shapeOfFields);
+  console.log(util.inspect(shapeOfFields, { showHidden: false, depth: null, colors: true }));
+}
